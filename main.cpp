85a7735a803dd7db1f6ffc5199a8c44dd63f8977
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-11-13 18:19:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-    int n=0;
+    int n=0, a=0, b=1, c=0;
     cin>>n;
-    if(n<2; cout<<"errore"<<endl);
-        
-    else{
+    if(n>=2){
         cout<<"1"<<endl;
-        int a=0, b=1, c=0;
-        for (int i=0; i<=(n-2); i++)
-        cout<<c;
-      {  c=a+b;
+        for (int i=0; i<n; i++){
+        cout<<c<<endl;
+         c=a+b;
          a=b;
          b=c;
-      }
+         }
+    } else{
+         cout<<"errore"<<endl;
+        
         }
         return 0;
 }

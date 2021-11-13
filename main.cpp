dcc715d51503154693bb6cb2e6fe8da1a4ec97a8
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-11-13 12:13:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@ int main() {
     cin>>n;
     if(n<2){
         cout<<"errore"<<endl;
-    }
+           }
     else{
         cout<<"1"<<endl;
         int a=0, b=1, c=0;
         for (int i=0; i<=(n-2); i++);
         cout<<c;
-        {   c=a+b;
-            a=b;
-            b=c;
+      {  c=a+b;
+         a=b;
+         b=c;
+      }
         }
-    }
+        return 0;
+}
